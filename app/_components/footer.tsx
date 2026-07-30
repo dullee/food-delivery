51b13967b2a8b6fb62ad78ffba18f60ec55fde52
dullee <91831552+dullee@users.noreply.github.com>
2026-07-30 10:11:09 +0800

    Enhance footer styling and add new logo
    - Updated footer text color for better visibility.
    - Added border to the footer for improved separation.
    - Cleaned up import statements for consistency.
    - Introduced new logo (logoH.svg) to replace the previous one.

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
 import logo from "@/public/logo.svg";
-import instaIcon from "@/public/instagramIcon.svg"
-import faceIcon from "@/public/facebookIcon.svg"
+import instaIcon from "@/public/instagramIcon.svg";
+import faceIcon from "@/public/facebookIcon.svg";
 
 export default function Footer() {
   return (
@@ -13,14 +13,14 @@ export default function Footer() {
           <Image alt="logo" src={logo} />
           <div className="flex  md:gap-28 flex-row">
             <div className="flex flex-col gap-4">
-              <p>NOMNOM</p>
+              <p className="text-[#71717A]">NOMNOM</p>
               <p>Home</p>
               <p>Contact us</p>
               <p>Delivery zone</p>
             </div>
             <div className="flex flex-row md:gap-14">
               <div className="flex flex-col md:gap-4">
-                <p>MENU</p>
+                <p className="text-[#71717A]">MENU</p>
                 <p>Appetizers</p>
                 <p>Salads</p>
                 <p>Pizzas</p>
@@ -39,16 +39,13 @@ export default function Footer() {
             <div className="flex flex-col gap-4">
               <p className="text-[#71717A]">FOLLOW US</p>
               <div className="flex gap-4">
-
-              <Image alt="facebook" src={faceIcon}/>
-              <Image alt="instagram" src={instaIcon}/>
-
+                <Image alt="facebook" src={faceIcon} />
+                <Image alt="instagram" src={instaIcon} />
               </div>
-
             </div>
           </div>
         </div>
-        <div className="flex py-8 md:gap-12">
+        <div className="flex py-8 md:gap-12 border-t text-[#71717A]">
           <p>Copy right 2024 © Nomnom LLC</p>
 
           <p>Privacy policy</p>
