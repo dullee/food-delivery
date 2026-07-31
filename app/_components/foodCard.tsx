240df1f2be154f32261382a9ae687debaca2cc55
dullee <91831552+dullee@users.noreply.github.com>
2026-08-01 00:20:57 +0800

    feat: implement addToCart function and update FoodCard component to display food details

--- app/_components/foodCard.tsx
+++ app/_components/foodCard.tsx
@@ -1,5 +1,6 @@
-import fingerFoodImg from "@/public/finger-food.jpg";
+"use client";
 import Image from "next/image";
+import axios from "axios";
 
 import {
   Card,
@@ -13,25 +14,72 @@ import {
 import { Button } from "@base-ui/react";
 import { Plus } from "lucide-react";
 
-function addToCart(id) {}
+export interface FoodItem {
+  foodName: string;
+  price: number;
+  image?: string;
+  ingredients?: string;
+  category: { categoryName: string };
+}
+export const allFoodObj: Record<number, FoodItem> = {
+  0: {
+    foodName: "Steak",
+    price: 50,
+    ingredients: "meat",
+    image:
+      "https://plus.unsplash.com/premium_photo-1723478557023-1f739ec06671?q=80&w=2272&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: { categoryName: "meat" },
+  },
+};
 
-export default function FoodCard(id: number) {
+async function addToCart(food_id: number) {
+  try {
+    const selectedFood = allFoodObj[food_id];
+
+    if (!selectedFood) {
+      console.error(`Food item with ID ${food_id} not found locally.`);
+      return;
+    }
+
+    // 2. Use the spread operator (...) to send fields directly matching the Mongoose schema
+    await axios.post("http://localhost:4000/api/foods", {
+      ...selectedFood,
+    });
+
+    console.log("Food added successfully!");
+  } catch (error: any) {
+    const errorMsg =
+      error.response?.data?.error || error.message || "Unknown error";
+    console.log("Error:", errorMsg);
+  }
+}
+interface FoodCardProps {
+  id: number;
+}
+export default function FoodCard({ id }: FoodCardProps) {
+  const food = allFoodObj[id];
+
   return (
     <Card className="flex p-4">
       <div className="relative">
-        <Image alt="finger food" src={fingerFoodImg} />
+        <Image
+          alt="finger food"
+          src={food.image || "/finger-food.png"}
+          width={200}
+          height={200}
+        />
         <Button
-          onClick={addToCart}
-          className={"absolute bottom-5 right-5 bg-white rounded-full p-3.5"}
+          onClick={() => addToCart(id)}
+          className={
+            "absolute bottom-5 right-5 bg-white cursor-pointer rounded-full p-3.5"
+          }
         >
           <Plus />
         </Button>
       </div>
       <div className="">
-        <h1>Finger Food</h1>
-        <CardDescription>
-          Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.
-        </CardDescription>
+        <h1>{food.foodName}</h1>
+        <CardDescription>{food.ingredients}</CardDescription>
       </div>
     </Card>
   );
--- app/_components/foodMenu.tsx
+++ app/_components/foodMenu.tsx
@@ -6,38 +6,38 @@ export default function FoodMenu() {
       <div>
         <h1 className="pb-13.5 text-white ">Appetizers</h1>
         <div className="grid md:grid-cols-3 md:gap-9">
+          <FoodCard id={0} />
+          {/* <FoodCard />
           <FoodCard />
           <FoodCard />
           <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
+          <FoodCard /> */}
         </div>
       </div>
       <div>
         <h1 className="pb-13.5 text-white ">Salads</h1>
         <div className="grid md:grid-cols-3 md:gap-9">
+          {/* <FoodCard />
           <FoodCard />
-          <FoodCard />
-          <FoodCard />
+          <FoodCard /> */}
         </div>
       </div>
       <div>
         <h1 className="pb-13.5 text-white ">Lunch favorites</h1>
         <div className="grid md:grid-cols-3 md:gap-9">
+          {/* <FoodCard />
           <FoodCard />
           <FoodCard />
           <FoodCard />
-          <FoodCard />
-          <FoodCard />
+          <FoodCard /> */}
         </div>
       </div>
       <div>
         <h1 className="pb-13.5 text-white ">Salads</h1>
         <div className="grid md:grid-cols-3 md:gap-9">
+          {/* <FoodCard />
           <FoodCard />
-          <FoodCard />
-          <FoodCard />
+          <FoodCard /> */}
         </div>
       </div>
     </div>
