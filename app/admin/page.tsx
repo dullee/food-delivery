236404025a9a6e8429d1dd4650fdf1b45957528a
dullee <91831552+dullee@users.noreply.github.com>
2026-08-04 21:02:14 +0800

    feat: refactor admin order management; implement DataTable and dropdown components for enhanced order display and interaction

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,45 +1,14 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, Truck } from "lucide-react";
 import { Pagination } from "@/components/ui/pagination";
-import AdminOrderInfo,  from "../_components/adminOrderInfo";
-
-import { ColumnDef } from "@tanstack/react-table";
-
-type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
-
-export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
-];
-
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-  ]
+import AdminOrderInfo from "./_components/adminOrderInfo";
+import { useState } from "react";
 
 export default function Page() {
+  const [selectedMenu, setSelectedMenu] = useState<string>("orders");
+
   return (
     <div className="flex min-h-screen bg-[#F4F4F5]">
       <div className="max-w-360 flex w-full relative gap-6">
@@ -49,10 +18,18 @@ export default function Page() {
               <Image alt="logo" width={165} height={44} src={"logoHDark.svg"} />
             </div>
             <div className="flex flex-col gap-6 items-center">
-              <h1 className="flex gap-2.5 py-2 px-6">
+              <Button
+                variant={"ghost"}
+                onClick={() => setSelectedMenu("menu")}
+                className={`py-2 px-6 gap-2.5 flex  ${selectedMenu === "menu" && "bg-black text-white"} `}
+              >
                 <LayoutDashboard /> Food menu
-              </h1>
-              <Button className={"py-2 px-6 gap-2.5 flex"}>
+              </Button>
+              <Button
+                variant={"ghost"}
+                onClick={() => setSelectedMenu("orders")}
+                className={`py-2 px-6 gap-2.5 flex  ${selectedMenu === "orders" && "bg-black text-white"} `}
+              >
                 <Truck />
                 <h2 className="w-full">Orders</h2>
               </Button>
@@ -63,8 +40,8 @@ export default function Page() {
         <div className="mt-6 mr-10 w-full flex flex-col gap-6">
           <div className="ml-auto">Pf picture</div>
           <div>
+            {selectedMenu === "orders" ? <AdminOrderInfo /> : null}
 
-            <AdminOrderInfo columns={columns} data={data}/>
             <Pagination></Pagination>
           </div>
         </div>
